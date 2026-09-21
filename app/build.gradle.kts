3d533a0e4ea5c61b6deb3e379a3c1ba6d3f6bf2e
Dahyun Park <howielyn23@gmail.com>
2026-09-21 12:37:49 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,7 +93,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.simple.tools.commons)
+    implementation(libs.commons)
     implementation(libs.eventbus)
     implementation(libs.audiorecordview)
     implementation(libs.androidx.documentfile)
